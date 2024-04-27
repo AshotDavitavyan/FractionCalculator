024ab265c938576671bbc445cfc89eea9be9dc6d
ashot <ashot@ACERASPIRE5>
2024-04-27 19:56:00 +0400

    Add project files.

---
+++ Fraction.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FractionCalculator
+{
+	internal class Fraction
+	{
+		private int _numerator;
+		private int _denominator;
+
+		public Fraction(int numerator, int denominator)
+		{
+			try
+			{
+				Numerator = numerator;
+				Denominator = denominator;
+			}
+			catch (ArgumentException ex)
+			{
+				throw;
+			}
+		}
+
+		public int Numerator
+		{
+			get { return _numerator; }
+			set { _numerator = value; }
+		}
+		public int Denominator
+		{
+			get { return _denominator; }
+			set
+			{
+				if (value == 0)
+					throw new ArgumentException("Denominator Cannot be equal to 0");
+
+				_denominator = value;
+			}
+		}
+
+		public static int GCD(int num1, int num2)
+		{
+			int biggerOne = num1 > num2 ? num1 : num2;
+			while (biggerOne % num1 != 0 && biggerOne % num2 != 0)
+			{
+				biggerOne++;
+			}
+			return biggerOne;
+		}
+
+		public static int LCM(int num1, int num2)
+		{
+			int smallerOne = num1 < num2 ? num1 : num2;
+			while (smallerOne != 0 && num1 % smallerOne != 0 && num2 % smallerOne != 0)
+			{
+				smallerOne--;
+			}
+			return smallerOne;
+		}
+
+		public static Fraction operator +(Fraction first, Fraction second)
+		{
+			int commonDenominator = LCM(first.Denominator, second.Denominator);
+
+			int newNumerator1 = first.Numerator * (commonDenominator / second.Denominator);
+			int newNumerator2 = second.Numerator * (commonDenominator / first.Denominator);
+
+			int resultNumerator = (newNumerator1 + newNumerator2);
+			return new Fraction(resultNumerator, commonDenominator);
+		}
+
+		public static Fraction operator -(Fraction first, Fraction second)
+		{
+			int commonDenominator = LCM(first.Denominator, second.Denominator);
+
+			int newNumerator1 = first.Numerator * (commonDenominator / second.Denominator);
+			int newNumerator2 = second.Numerator * (commonDenominator / first.Denominator);
+
+			int resultNumerator = (newNumerator1 - newNumerator2);
+			return new Fraction(resultNumerator, commonDenominator);
+		}
+
+		public static Fraction operator *(Fraction first, Fraction second)
+		{
+			int newNumerator = first.Numerator * second.Numerator;
+			int newDenominator = first.Denominator * second.Denominator;
+
+			return new Fraction(newNumerator, newDenominator);
+		}
+
+		public static Fraction operator /(Fraction first, Fraction second)
+		{
+			int tmp = second.Numerator;
+			second.Numerator = second.Denominator;
+			second.Denominator = tmp;
+			return first * second;
+		}
+
+	}
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FractionCalculator
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			firstNM.Text = string.Empty;
+			firstDN.Text = string.Empty;
+			secondNM.Text = string.Empty;
+			secondDN.Text = string.Empty;
+			resultNM.Text = string.Empty;
+			resultDN.Text = string.Empty;
+		}
+
+		public int ConvertValue(string? value)
+		{
+			if (int.TryParse(value, out int valueInt))
+			{
+				return valueInt;
+			}
+			else
+			{
+				throw new ArgumentException("Invalid value");
+			}
+		}
+
+
+		private Fraction CalculateValue(Fraction fraction1, Fraction fraction2)
+		{
+			switch (action.Text)
+			{
+				case "+":
+					return fraction1 + fraction2;
+				case "-":
+					return fraction1 - fraction2;
+				case "*":
+					return fraction1 * fraction2;
+				case "/":
+					return fraction1 / fraction2;
+				default:
+					throw new ArgumentException("Invalid action");
+			}
+		}
+
+		private Fraction ConvertToFraction(string numeratorStr, string denominatorStr)
+		{
+			int numerator = ConvertValue(numeratorStr);
+			int denominator = ConvertValue(numeratorStr);
+			return new Fraction(numerator, denominator);
+		}
+
+		private void Button_Click_1(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				Fraction fraction1 = ConvertToFraction(firstNM.Text, firstDN.Text);
+				Fraction fraction2 = ConvertToFraction(firstNM.Text, firstDN.Text);
+				Fraction answerFraction = CalculateValue(fraction1, fraction2);
+				resultNM.Text = answerFraction.Numerator.ToString();
+				resultDN.Text = answerFraction.Denominator.ToString();
+			}
+			catch (ArgumentException ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+	}
+}
